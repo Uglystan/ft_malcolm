7db4d012f65c620528ecd22811ecff3434efaa8b
Lucas Girault <117684814+Uglystan@users.noreply.github.com>
2024-04-05 15:53:48 +0200

    Add files via upload

---
+++ reseau/client2/script.sh
@@ -0,0 +1,41 @@
+tcpdump: verbose output suppressed, use -v[v]... for full protocol decode
+listening on eth0, link-type EN10MB (Ethernet), snapshot length 262144 bytes
+17:49:29.354072 ARP, Request who-has client1.ft_malcolm_my_network tell 8e067382eb0e, length 28
+17:49:29.354174 ARP, Reply client1.ft_malcolm_my_network is-at 02:42:c0:a8:01:02 (oui Unknown), length 28
+17:49:29.354178 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 1, length 64
+17:49:29.354250 IP client1.ft_malcolm_my_network > 8e067382eb0e: ICMP echo reply, id 1, seq 1, length 64
+17:49:30.380887 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 2, length 64
+17:49:30.380963 IP client1.ft_malcolm_my_network > 8e067382eb0e: ICMP echo reply, id 1, seq 2, length 64
+17:49:31.355121 ARP, Reply client1.ft_malcolm_my_network is-at 02:42:c0:a8:01:04 (oui Unknown), length 28
+17:49:31.404815 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 3, length 64
+17:49:32.433392 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 4, length 64
+17:49:33.452864 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 5, length 64
+17:49:34.480801 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 6, length 64
+17:49:34.480816 ARP, Request who-has 8e067382eb0e tell client1.ft_malcolm_my_network, length 28
+17:49:34.480833 ARP, Reply 8e067382eb0e is-at 02:42:c0:a8:01:03 (oui Unknown), length 28
+17:49:35.504735 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 7, length 64
+17:49:35.504769 IP client1.ft_malcolm_my_network > 8e067382eb0e: ICMP echo reply, id 1, seq 7, length 64
+17:49:36.524895 IP 8e067382eb0e > client1.ft_malcolm_my_network: ICMP echo request, id 1, seq 8, length 64
+17:49:36.524982 IP client1.ft_malcolm_my_network > 8e067382eb0e: ICMP echo reply, id 1, seq 8, length 64
+17:49:40.620937 ARP, Request who-has client1.ft_malcolm_my_network tell 8e067382eb0e, length 28
+17:49:40.621012 ARP, Reply client1.ft_malcolm_my_network is-at 02:42:c0:a8:01:02 (oui Unknown), length 28
+17:49:47.008414 IP 8e067382eb0e.44360 > 10.0.2.3.53: 63712+ PTR? 1.1.168.192.in-addr.arpa. (42)
+17:49:47.010040 IP 10.0.2.3.53 > 8e067382eb0e.44360: 63712 NXDomain 0/1/0 (91)
+17:49:47.105838 IP 8e067382eb0e.36084 > 10.0.2.3.53: 13289+ PTR? 3.2.0.10.in-addr.arpa. (39)
+17:49:47.110531 IP 10.0.2.3.53 > 8e067382eb0e.36084: 13289 ServFail 0/0/0 (39)
+17:49:47.114526 IP 8e067382eb0e.50463 > 10.0.2.3.53: 13289+ PTR? 3.2.0.10.in-addr.arpa. (39)
+17:49:47.117643 IP 10.0.2.3.53 > 8e067382eb0e.50463: 13289 ServFail 0/0/0 (39)
+17:49:47.119074 IP 8e067382eb0e.32868 > 10.0.2.3.53: 13289+ PTR? 3.2.0.10.in-addr.arpa. (39)
+17:49:47.122578 IP 10.0.2.3.53 > 8e067382eb0e.32868: 13289 ServFail 0/0/0 (39)
+17:49:49.608422 IP 8e067382eb0e.53512 > 10.0.2.3.53: 13289+ PTR? 3.2.0.10.in-addr.arpa. (39)
+17:49:49.610935 IP 10.0.2.3.53 > 8e067382eb0e.53512: 13289 ServFail 0/0/0 (39)
+17:49:49.611336 IP 8e067382eb0e.45785 > 10.0.2.3.53: 13289+ PTR? 3.2.0.10.in-addr.arpa. (39)
+17:49:49.613135 IP 10.0.2.3.53 > 8e067382eb0e.45785: 13289 ServFail 0/0/0 (39)
+17:49:49.613665 IP 8e067382eb0e.32988 > 10.0.2.3.53: 13289+ PTR? 3.2.0.10.in-addr.arpa. (39)
+17:49:49.616627 IP 10.0.2.3.53 > 8e067382eb0e.32988: 13289 ServFail 0/0/0 (39)
+17:49:52.141638 ARP, Request who-has 192.168.1.1 tell 8e067382eb0e, length 28
+17:49:52.141701 ARP, Reply 192.168.1.1 is-at 02:42:a0:d6:09:75 (oui Unknown), length 28
+17:49:52.145037 ARP, Request who-has 8e067382eb0e tell 192.168.1.1, length 28
+17:49:52.145052 ARP, Reply 8e067382eb0e is-at 02:42:c0:a8:01:03 (oui Unknown), length 28
+17:49:52.229769 IP 8e067382eb0e.54862 > 10.0.2.3.53: 44151+ PTR? 1.1.168.192.in-addr.arpa. (42)
+17:49:52.232676 IP 10.0.2.3.53 > 8e067382eb0e.54862: 44151 NXDomain 0/1/0 (91)
---
+++ reseau/pirate/src/utils.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+size_t ft_strlen(const char *str)
+{
+    size_t i = 0;
+    for(; str[i] != '\0'; i++)
+    {
+    }
+    return (i);
+}
+
+void ft_strcpy(char *dest, const char *src)
+{
+    size_t i = 0;
+    for (; i < ft_strlen(src); i++)
+    {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+}
+
+// Transform data en binaire
+// Je met le bit en position la plus faible et je le compare avec 1 donc je l'extrait et ensuite je le met dans une string d'ou le +48
+// Dans cette fonction je vais extraire bit par bit ce que j'ai recu dans recvfrom donc je decale le chaque bit de chaque octect pour le mettre en position la plus faible
+// Et ensuite avec le AND 1 je l'extrait et comme je le met dans une chaine de caractere je fais +48
+void converToBinary(char *data, size_t length)
+{
+    char binTrame[SIZE_MAX_ARP];
+    size_t i = 0, k = 0;
+    for (; i < length; ++i) {
+        for (int j = 7; j >= 0; --j) {
+            binTrame[k++] = ((data[i] >> j) & 1) + 48;
+        }
+    }
+    binTrame[k] = '\0';
+    ft_strcpy(data, binTrame);
+}
+
+void binaryToHex(char *binStr)
+{
+    char hexStr[SIZE_MAX_ARP];
+    size_t i = 0, j = 0, tot = 0;
+    while(binStr[i] != '\0')
+    {
+        if(i % 4 == 0)
+            tot = 0;
+        if (binStr[i] == '1')
+        {
+            if((i + 1) % 4 == 0)
+                tot = tot + 1;
+            else if((i + 1) % 4 == 3)
+                tot = tot + 2;
+            else if((i + 1) % 4 == 2)
+                tot = tot + 4;
+            else if((i + 1) % 4 == 1)
+                tot = tot + 8;
+        }
+        if((i + 1) % 4 == 0)
+            hexStr[j++] = "0123456789ABCDEF"[tot];
+        i++;
+    }
+    hexStr[j] = '\0';
+    ft_strcpy(binStr, hexStr);
+}
+
+int pos_ascii_hex_int_to_int(char *str, size_t base_size)
+{
+    int ret = 0;
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+            ret = (ret * base_size) + (str[i] - '0');
+        else
+            ret = (ret * base_size) + (str[i] - 'a' + 10);
+    }
+    return (ret);
+}
